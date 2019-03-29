712388541014f542a640734543b1dff160bcc07a
macaba <macaba@gmail.com>
2019-03-29 18:15:17 +0000

    Added calibration routine

--- AFSTwoTwoSine/main/main.cpp
+++ AFSTwoTwoSine/main/main.cpp
@@ -6,9 +6,11 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
 
 AudioControlI2S          i2s;
 AudioControlPCM3060      pcm3060;
+AudioControlAFSTwoTwo    afs22;
 
 // GUItool: begin automatically generated code
 AudioSynthWaveformSine   sine1;          //xy=398,548
@@ -35,6 +37,139 @@ void audioTask( void * parameter )
   }
 }
 
+void calibrationTask (void * parameter)
+{
+  static const char *TAG = "CalibrationTask";
+  const int delayMs = 100;
+  float in0V, in5V, out0V, out5V;
+  calibrationInL.enableAverage();
+
+  //Input L calibration
+  calibrationInL.inputNormal();
+  ESP_LOGI(TAG, "Setting left input to 0V.");
+  afs22.setLeftInputChannelTo0V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in0V = calibrationInL.getInputAverage(); 
+  ESP_LOGI(TAG, "Setting left input to 5V.");
+  afs22.setLeftInputChannelTo5V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in5V = calibrationInL.getInputAverage();
+  ESP_LOGI(TAG, "Left input 0V: %.4f, 5V: %.4f", in0V * 10, in5V * 10);
+  if(in0V < 0.05 && in0V > -0.05 && in5V > 0.45 && in5V < 0.55)
+    calibrationInL.calibrate(0, in0V, 0.5, in5V);
+
+  ESP_LOGI(TAG, "Setting left input to 0V.");
+  afs22.setLeftInputChannelTo0V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in0V = calibrationInL.getOutputAverage(); 
+  ESP_LOGI(TAG, "Setting left input to 5V.");
+  afs22.setLeftInputChannelTo5V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in5V = calibrationInL.getOutputAverage();
+  ESP_LOGI(TAG, "Left input 0V: %.4f, 5V: %.4f", in0V * 10, in5V * 10);
+  if(in0V < 0.001 && in0V > -0.001 && in5V > 0.499 && in5V < 0.501)
+    ESP_LOGI(TAG, "Input L calibration confirmed.");
+  else
+    ESP_LOGI(TAG, "Input L calibration failed.");
+  
+  //Output L calibration
+  afs22.setLeftInputChannelToOutL();
+  ESP_LOGI(TAG, "Setting left input to Out L at 0V.");
+  calibrationOutL.inputDC(0.0);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out0V = calibrationInL.getOutputAverage();
+  ESP_LOGI(TAG, "Setting left input to Out L at 5V.");
+  calibrationOutL.inputDC(0.5);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out5V = calibrationInL.getOutputAverage();
+  ESP_LOGI(TAG, "Left output 0V: %.4f, 5V: %.4f", out0V * 10, out5V * 10);
+  if(out0V < 0.05 && out0V > -0.05 && out5V > 0.45 && out5V < 0.55) 
+    calibrationOutL.calibrate(0, out0V, 0.5, out5V);
+
+  ESP_LOGI(TAG, "Setting left input to Out L at 0V.");
+  afs22.setLeftInputChannelToOutL();
+  calibrationOutL.inputDC(0.0);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out0V = calibrationInL.getOutputAverage();
+  ESP_LOGI(TAG, "Setting left input to Out L at 5V.");
+  calibrationOutL.inputDC(0.5);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out5V = calibrationInL.getOutputAverage();
+  ESP_LOGI(TAG, "Left output 0V: %.4f, 5V: %.4f", out0V * 10, out5V * 10);
+  if(out0V < 0.001 && out0V > -0.001 && out5V > 0.499 && out5V < 0.501) 
+    ESP_LOGI(TAG, "Output L calibration confirmed.");
+  else
+    ESP_LOGI(TAG, "Output L calibration failed.");
+
+  calibrationOutL.inputNormal();
+  calibrationInL.disableAverage();
+  calibrationInR.enableAverage();
+
+  //Input R calibration
+  calibrationInR.inputNormal();
+  ESP_LOGI(TAG, "Setting right input to 0V.");
+  afs22.setRightInputChannelTo0V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in0V = calibrationInR.getInputAverage(); 
+  ESP_LOGI(TAG, "Setting right input to 5V.");
+  afs22.setRightInputChannelTo5V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in5V = calibrationInR.getInputAverage();
+  ESP_LOGI(TAG, "Right input 0V: %.4f, 5V: %.4f", in0V * 10, in5V * 10);
+  if(in0V < 0.05 && in0V > -0.05 && in5V > 0.45 && in5V < 0.55) 
+    calibrationInR.calibrate(0, in0V, 0.5, in5V);
+
+  ESP_LOGI(TAG, "Setting right input to 0V.");
+  afs22.setRightInputChannelTo0V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in0V = calibrationInR.getOutputAverage(); 
+  ESP_LOGI(TAG, "Setting right input to 5V.");
+  afs22.setRightInputChannelTo5V();
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  in5V = calibrationInR.getOutputAverage();
+  ESP_LOGI(TAG, "Right input 0V: %.4f, 5V: %.4f", in0V * 10, in5V * 10);
+  if(in0V < 0.001 && in0V > -0.001 && in5V > 0.499 && in5V < 0.501)
+    ESP_LOGI(TAG, "Input R calibration confirmed.");
+  else
+    ESP_LOGI(TAG, "Input R calibration failed.");
+
+  //Output R calibration
+  afs22.setRightInputChannelToOutR();
+  ESP_LOGI(TAG, "Setting right input to Out R at 0V.");
+  calibrationOutR.inputDC(0.0);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out0V = calibrationInR.getOutputAverage();
+  ESP_LOGI(TAG, "Setting right input to Out R at 5V.");
+  calibrationOutR.inputDC(0.5);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out5V = calibrationInR.getOutputAverage();
+  ESP_LOGI(TAG, "Right output 0V: %.4f, 5V: %.4f", out0V * 10, out5V * 10);
+  if(out0V < 0.05 && out0V > -0.05 && out5V > 0.45 && out5V < 0.55) 
+    calibrationOutR.calibrate(0, out0V, 0.5, out5V);
+
+  afs22.setRightInputChannelToOutR();
+  ESP_LOGI(TAG, "Setting right input to Out R at 0V.");
+  calibrationOutR.inputDC(0.0);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out0V = calibrationInR.getOutputAverage();
+  ESP_LOGI(TAG, "Setting right input to Out R at 5V.");
+  calibrationOutR.inputDC(0.5);
+  vTaskDelay(delayMs/portTICK_RATE_MS);
+  out5V = calibrationInR.getOutputAverage();
+  ESP_LOGI(TAG, "Right output 0V: %.4f, 5V: %.4f", out0V * 10, out5V * 10);
+  if(out0V < 0.001 && out0V > -0.001 && out5V > 0.499 && out5V < 0.501) 
+    ESP_LOGI(TAG, "Output R calibration confirmed.");
+  else
+    ESP_LOGI(TAG, "Output R calibration failed.");
+
+  calibrationOutR.inputNormal();
+  calibrationInR.disableAverage();
+  afs22.setInputsToNormal();
+  ESP_LOGI(TAG, "Inputs set to input pins.");
+
+  vTaskDelete( NULL );
+}
+
 extern "C" {
    void app_main();
 }
@@ -58,14 +193,12 @@ void app_main()
     i2s.default_codec_rx_tx_24bit();
     vTaskDelay(1000/portTICK_RATE_MS);
     pcm3060.init();
-    sine1.frequency(440);
+
+    sine1.frequency(220);
     sine1.amplitude(0.1);
-    sine2.frequency(880);
+    sine2.frequency(440);
     sine2.amplitude(0.1);
-    calibrationOutL.calibrate(0, 0.0156, 10, 11.34);
-    calibrationOutR.calibrate(0, 0.069, 10, 11.372);
-    calibrationInL.calibrate(0, -0.000757, 1, 0.958241);
-    calibrationInR.calibrate(0, -0.000757, 1, 0.958241);    //Made up these values
  
     xTaskCreatePinnedToCore(audioTask, "AudioTask", 10000, NULL, configMAX_PRIORITIES - 1, NULL, 1);
+    xTaskCreatePinnedToCore(calibrationTask, "CalibrationTask", 2000, NULL, 1, NULL, 1);
 }
